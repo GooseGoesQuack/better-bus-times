48d1436676d983a4752002cc0aa294cf3f438939
Hongyu An <hongyuan22222@gmail.com>
2022-12-25 13:02:46 +0000

    Add dynamic generation of new departures

--- scripts.js
+++ scripts.js
@@ -17,25 +17,65 @@ function get_times() {
                 var line = value.lineName;
                 var destination = value.destinationName;
                 var time = value.timeToStation;
+                var vehicle = value.vehicleId;
                 time = parseInt(time/60);
                 time < 1 ? time = "Due" : time = time +" min";
-                addTime(line, destination, time)
+                addTime(vehicle, line, destination, time)
             });
         }
     });
 }
 
-function addTime(line, destination, time) {
-    // create a new div element
-    const newDiv = document.createElement("div");
-  
-    // and give it some content
-    const newContent = document.createTextNode(line +" to "+ destination +" - "+ time);
-  
-    // add the text node to the newly created div
-    newDiv.appendChild(newContent);
-  
-    // add the newly created element and its content into the DOM
-    const currentDiv = document.getElementById("search");
-    document.body.insertBefore(newDiv, currentDiv);
+function new_li(vehicle, line, destination, time) {
+}
+
+function addTime(vehicle, line, destination, time) {
+    var newDiv = Object.assign(
+        document.createElement(`div`), 
+        { class: `arrival flex`, 
+        innerHTML: `<span class="line">154</span>
+        <span class="destination">West Croydon</span>
+        <span class="eta">15</span><span class="min">min</span>`});
+      document.body.appendChild(newElem);
   }
+
+function addTime2(){
+    var container = document.getElementById('depart-container')
+
+    var newDiv = document.createElement("div");
+    newDiv.classList.add("arrival");
+    newDiv.classList.add("flex");
+    container.appendChild(newDiv);
+
+    var newLine = document.createElement("span");
+    newLine.classList.add("line");
+    var lineContent = document.createTextNode("154");
+    newLine.appendChild(lineContent);
+    newDiv.appendChild(newLine);
+
+    var newDest = document.createElement("span");
+    newDest.classList.add("destination");
+    var destContent = document.createTextNode("West Croydon");
+    newDest.appendChild(destContent);
+    newDiv.appendChild(newDest);
+
+    var newETA = document.createElement("span");
+    newETA.classList.add("eta");
+    var ETAContent = document.createTextNode("15");
+    newETA.appendChild(ETAContent);
+    newDiv.appendChild(newETA);
+
+    var newMin = document.createElement("span");
+    newMin.classList.add("min");
+    var minContent = document.createTextNode("min");
+    newMin.appendChild(minContent);
+    newDiv.appendChild(newMin);
+}
+
+function test(){
+    var newDiv = document.createElement("div");
+    newDiv.innerText = "This is a div";
+
+    var container = document.getElementById('depart-container')
+    container.appendChild(newDiv);
+}
